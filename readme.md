0a36e6e75ca2a8176ebfcae07e08eab45193c420
hcd1983 <hcd@mojopot.com>
2020-06-01 02:37:20 +0800

    readme

---
+++ readme.md
@@ -0,0 +1,35 @@
+![alt text](https://it-monk.com/wp-content/uploads/2020/05/cropped-ITMOCK_Logo-03-1-2048x277.png)
+
+
+# WooCommerce 結帳頁面嵌入退換貨政策
+
+ 
+## 開發教學文章: [WooCommerce 結帳頁面嵌入退換貨政策](https://it-monk.com/自製-wp-外掛-woocommerce-結帳頁面嵌入退換貨政策/) ##
+
+
+![picture alt](https://it-monk.com/wp-content/uploads/2020/06/done-01.jpg)
+
+## 安裝步驟 ##
+
+### 1. 下載整包 zip ###
+
+![picture alt](https://it-monk.com/wp-content/uploads/2020/05/aabbcc.png "it monk")
+
+### 2. 進入 WordPress 後台 ###
+
+外掛 >> 安裝外掛 >> 上傳外掛
+ 
+![picture alt](https://it-monk.com/wp-content/uploads/2020/05/擷取-2.jpg)
+
+
+### 3. 啟用 ###
+![picture alt](https://it-monk.com/wp-content/uploads/2020/06/未命名-1-01.jpg)
+
+啟用外掛後即可。
+
+## 使用方式 ##
+
+### 1. 啟用後，後台會多出 wooCommerce 退貨政策目錄 ###
+
+進入編輯即可。
+![picture alt](https://it-monk.com/wp-content/uploads/2020/06/abcd.jpg)
--- woo-refund-policy.php
+++ woo-refund-policy.php
@@ -5,108 +5,109 @@ Author: huanyichuang, IT-MONK
 Description:  woocommerce "退貨政策" 的自訂欄位，在 WooCommerce 結帳頁顯示內容。
 */
 
-//$plugin_slug = "woo-refund-policy";
 
 
+// 註冊設定欄位
+add_action( 'admin_init', 'woo_refund_policy_option_register' );
+function woo_refund_policy_option_register(){
 
-    add_action( 'admin_init', 'woo_refund_policy_option_register' );
-    function woo_refund_policy_option_register(){
+    register_setting( 'woo_refund_policy', 'woo_refund_policy_title' );
+    register_setting( 'woo_refund_policy', 'woo_refund_policy' );
 
-        register_setting( 'woo_refund_policy', 'woo_refund_policy_title' );
-        register_setting( 'woo_refund_policy', 'woo_refund_policy' );
+}
 
-    }
+// 在 wordpress 的管理者目錄新增選項
+add_action( 'admin_menu', 'woo_refund_policy_settings_page' );
+function woo_refund_policy_settings_page() {
+    $title = esc_html__( 'wooCommerce 退貨政策', "woo-refund-policy" );
+    add_menu_page( $title , $title, 'edit_posts', "woo-refund-policy", 'woo_refund_policy_ui' );
+}
 
-    add_action( 'admin_menu', 'woo_refund_policy_settings_page' );
-    function woo_refund_policy_settings_page() {
-        $title = esc_html__( 'woocommerce 退貨政策', "woo-refund-policy" );
-        add_menu_page( $title , $title, 'edit_posts', "woo-refund-policy", 'woo_refund_policy_ui' );
-    }
+// 介面設計
+function woo_refund_policy_ui() {
+    $woo_refund_policy_txt = get_option('woo_refund_policy');
+    $woo_refund_policy_title = get_option('woo_refund_policy_title');
+    ?>
+    <div class="wrap">
+        <h1><?php echo _e( 'WooCommerce 退貨政策設定', "woo-refund-policy" ); ?></h1>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'woo_refund_policy' ); ?>
+            <table class="form-table" role="presentation">
+                <tbody>
+                <tr>
+                    <th scope="row"><label for="blogname"><?php echo _e( 'WooCommerce 退貨政策標題', "woo-refund-policy" ); ?></label></th>
+                    <td>
+                        <input name="woo_refund_policy_title" type="text"  value="<?php echo $woo_refund_policy_title;?>" class="regular-text">
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="blogname"><?php echo _e( 'WooCommerce 退貨政策', "woo-refund-policy" ); ?></label></th>
+                    <td>
+                        <?php
+                        wp_editor (
+                            $woo_refund_policy_txt ,
+                            'woo_refund_policy',
+                            array ( "media_buttons" => true, 'textarea_name' => 'woo_refund_policy','textarea_rows' => 3 )
+                        );
+                        ?>
+                    </td>
+                </tr>
+            </table>
 
-    function woo_refund_policy_ui() {
-        $woo_refund_policy_txt = get_option('woo_refund_policy');
-        $woo_refund_policy_title = get_option('woo_refund_policy_title');
-        ?>
-        <div class="wrap">
-            <h1><?php echo _e( 'woocommerce 退貨政策設定', "woo-refund-policy" ); ?></h1>
-            <form method="post" action="options.php">
-                <?php settings_fields( 'woo_refund_policy' ); ?>
-                <table class="form-table" role="presentation">
-                    <tbody>
-                    <tr>
-                        <th scope="row"><label for="blogname"><?php echo _e( 'woocommerce 退貨政策標題', "woo-refund-policy" ); ?></label></th>
-                        <td>
-                            <input name="woo_refund_policy_title" type="text"  value="<?php echo $woo_refund_policy_title;?>" class="regular-text">
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><label for="blogname"><?php echo _e( 'woocommerce 退貨政策', "woo-refund-policy" ); ?></label></th>
-                        <td>
-                            <?php
-                            wp_editor (
-                                $woo_refund_policy_txt ,
-                                'woo_refund_policy',
-                                array ( "media_buttons" => true, 'textarea_name' => 'woo_refund_policy','textarea_rows' => 3 )
-                            );
-                            ?>
-                        </td>
-                    </tr>
-                </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
 
-                <?php submit_button(); ?>
-            </form>
-        </div>
-
-        <?php
-    }
+    <?php
+}
 
 
 
 //    結帳頁面新增 policy 欄位
-    add_action( 'woocommerce_review_order_before_submit', 'hyc_refund', 99 );
-    if ( ! function_exists( 'hyc_refund' ) ) {
-        function hyc_refund() {
-            $refund_policy_txt = get_option('woo_refund_policy');
-            $woo_refund_policy_title = get_option('woo_refund_policy_title');
-            if (  $refund_policy_txt ) {
+add_action( 'woocommerce_review_order_before_submit', 'hyc_refund', 99 );
+if ( ! function_exists( 'hyc_refund' ) ) {
+    function hyc_refund() {
+        $refund_policy_txt = get_option('woo_refund_policy');
+        $woo_refund_policy_title = get_option('woo_refund_policy_title');
+        if (  $refund_policy_txt ) {
+            ?>
+            <div id="refund" style="overflow-y: scroll; max-height: 10em; margin-bottom: 2em; background: #fff; padding: 15px;">
+                <?php
+                if($woo_refund_policy_title):
                 ?>
-                <div id="refund" style="overflow-y: scroll; max-height: 10em; margin-bottom: 2em; background: #fff; padding: 15px;">
-                    <?php
-                    if($woo_refund_policy_title):
-                    ?>
-                        <h3>
-                            <?php echo $woo_refund_policy_title; ?>
-                        </h3>
-                    <?php
-                    endif;
-                    ?>
+                    <h3>
+                        <?php echo $woo_refund_policy_title; ?>
+                    </h3>
+                <?php
+                endif;
+                ?>
 
-                    <?php
-//                        比照文章內容使用 filter ，讓 shortcode 也能正確顯示
-                        echo apply_filters( 'the_content',  $refund_policy_txt  );
-                    ?>
-                </div>
+                <?php
+                //比照文章內容使用 filter ，讓 shortcode 也能正確顯示
+                    echo apply_filters( 'the_content',  $refund_policy_txt  );
+                ?>
+            </div>
 
-            <?php } }
-    }
+        <?php } }
+}
 
 
 
 
+// 如果沒有安裝 WooCommerce 跳出提示
+add_action( 'admin_notices', 'pls_install_woocommerce' );
+function pls_install_woocommerce() {
 
-    add_action( 'admin_notices', 'pls_install_woocommerce' );
-    function pls_install_woocommerce() {
+    if(!class_exists( 'WooCommerce' )):
+    ?>
 
-        if(!class_exists( 'WooCommerce' )):
-        ?>
+    <div class="notice notice-warning is-dismissible">
+        <p><?php _e( '兄弟 (姊妹)，<strong>woo-refund-policy</strong> 外掛要先安裝 WooCommerce! 才會生效', "woo-refund-policy" ); ?></p>
+    </div>
 
-        <div class="notice notice-warning is-dismissible">
-            <p><?php _e( '兄弟 (姊妹)，<strong>woo-refund-policy</strong> 外掛要先安裝 WooCommerce! 才會生效', "woo-refund-policy" ); ?></p>
-        </div>
-
-        <?php
-        endif;
-    }
+    <?php
+    endif;
+}
 
 
 
